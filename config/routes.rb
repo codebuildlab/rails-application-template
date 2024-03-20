a0c47fc08f9a3d870a851193b2cbb7c1a39ff81b
Salvador Gascon <salvadorgascon@protonmail.com>
2024-03-20 13:10:10 +0100

    Generate devie controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,17 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, :path => "auth", controllers: {
+    sessions: 'auth/sessions',
+    confirmations: 'auth/confirmations',
+    passwords: 'auth/passwords',
+    registrations: 'auth/registrations',
+    unlocks: 'auth/unlocks',
+    omniauth: 'auth/omniauth',
+  }
+  
+  devise_scope :user do
+    # Extra methods for user 
+  end
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.

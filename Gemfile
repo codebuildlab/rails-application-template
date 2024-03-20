403b8f2b3e4b4972e332425ff366a5420c6aaa99
Salvador Gascon <salvadorgascon@protonmail.com>
2024-03-20 11:37:53 +0100

    Add shoulda matchers for rspec

--- Gemfile
+++ Gemfile
@@ -70,6 +70,11 @@ group :development, :test do
   gem 'factory_bot_rails'
 end
 
+group :test do
+  # Extensions for assertions
+  gem 'shoulda-matchers'
+end
+
 group :development do
   # Use console on exceptions pages [https://github.com/rails/web-console]
   gem 'web-console'
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -44,6 +44,13 @@ RSpec.configure do |config|
     config.include FactoryBot::Syntax::Methods
   end
 
+  Shoulda::Matchers.configure do |config|
+    config.integrate do |with|
+      with.test_framework :rspec
+      with.library :rails
+    end
+  end
+
   # You can uncomment this line to turn off ActiveRecord support entirely.
   # config.use_active_record = false
 

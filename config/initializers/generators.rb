6dafc4b33cfc5d7529006daffa2336448c50a3ff
Salvador Gascon <salvadorgascon@protonmail.com>
2024-03-20 16:54:57 +0100

    Improve generators defining rspec generators

--- config/initializers/generators.rb
+++ config/initializers/generators.rb
@@ -1,4 +1,14 @@
-Rails.application.config.generators do |g|
-  g.assets = false
-  g.helper = false
+Rails.application.config.generators do |g|  
+  g.helper false
+  g.decorator false
+
+  g.test_framework :rspec,
+                   fixtures: true,
+                   view_specs: true,
+                   helper_specs: true,
+                   routing_specs: true,
+                   controller_specs: true,
+                   request_specs: true
+                   
+  g.fixture_replacement :factory_girl, dir: "spec/factories"
 end

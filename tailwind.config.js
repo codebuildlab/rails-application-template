41f304eed2a40dbdf4535763c9fca70e15ee4c13
Salvador Gascon <salvadorgascon@protonmail.com>
2024-03-24 22:27:15 +0100

    Set default font family to Open Sans

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,13 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
+  theme:{
+    fontFamily:{
+      'sans': ["Open Sans", ...defaultTheme.fontFamily.sans],
+      // 'roboto': ['Roboto', ...defaultTheme.fontFamily.sans],
+      // 'poppins': ['Roboto', ...defaultTheme.fontFamily.sans]
+    }    
+  },
   content: [
     './app/views/**/*.html.erb',
     './app/helpers/**/*.rb',

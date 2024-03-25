0d5425e12b7c2dae0dbea6b259130e12a0f78caa
Salvador Gascon <salvadorgascon@protonmail.com>
2024-03-25 11:34:38 +0100

    Fix rubocop offenses

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,4 @@
-require "active_support/core_ext/integer/time"
+require 'active_support/core_ext/integer/time'
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
@@ -24,7 +24,7 @@ Rails.application.configure do
   # config.public_file_server.enabled = false
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
-  # config.asset_host = "http://assets.example.com"
+  # config.asset_host = 'http://assets.example.com'
 
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for Apache
@@ -47,23 +47,23 @@ Rails.application.configure do
 
   # Log to STDOUT by default
   config.logger = ActiveSupport::Logger.new(STDOUT)
-    .tap  { |logger| logger.formatter = ::Logger::Formatter.new }
-    .then { |logger| ActiveSupport::TaggedLogging.new(logger) }
+                                       .tap  { |logger| logger.formatter = Logger::Formatter.new }
+                                       .then { |logger| ActiveSupport::TaggedLogging.new(logger) }
 
   # Prepend all log lines with the following tags.
-  config.log_tags = [ :request_id ]
+  config.log_tags = [:request_id]
 
   # "info" includes generic and useful information about system operation, but avoids logging too much
   # information to avoid inadvertent exposure of personally identifiable information (PII). If you
   # want to log everything, set the level to "debug".
-  config.log_level = ENV.fetch("RAILS_LOG_LEVEL", "info")
+  config.log_level = ENV.fetch('RAILS_LOG_LEVEL', 'info')
 
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter = :resque
-  # config.active_job.queue_name_prefix = "rails_application_template_production"
+  # config.active_job.queue_name_prefix = 'rails_application_template_production'
 
   config.action_mailer.perform_caching = false
 
--- config/initializers/active_job.rb
+++ config/initializers/active_job.rb
@@ -3,21 +3,21 @@ Rails.application.config.active_job.queue_adapter = :sidekiq
 require 'sidekiq'
 require 'sidekiq/web'
 
-redis_url = ENV["REDIS_URL"]
+redis_url = ENV.fetch('REDIS_URL', nil)
 
 Sidekiq.configure_server do |config|
   config.redis = { url: redis_url }
 
   config.on(:startup) do
-    puts "Starting jobs ..."
+    puts 'Starting jobs ...'
   end
 
   config.on(:quiet) do
-    puts "Waiting for jobs ..."
+    puts 'Waiting for jobs ...'
   end
 
   config.on(:shutdown) do
-    puts "Stopping jobs ..."
+    puts 'Stopping jobs ...'
   end
 end
 
